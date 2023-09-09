04a7fb605e09a5879f4a3dcb9928c39e94c5a2d6
Nivetha s <111632916+Nivetha124@users.noreply.github.com>
2023-09-09 22:08:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Inventory from './Component/Inventory';
+// import Home from "./routes/Home;"
+
+// //import { Component } from "react";
+// import { Link } from "react-router-dom"
+// //import "./InventoryStyle.css";
+// //import {Menulist} from "./Menulist"
+
+export default function App() {
+  return (
+    <div className="App">
+      <Inventory/>
+    </div>
+  );
+}
+
+
